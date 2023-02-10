b0f5df4bb56f568b0f3d4963c0bdf7c48301be1d
davep80 <davidpaq1@gmail.com>
2023-02-10 09:41:44 +0000

    dep

--- server/src/app.py
+++ server/src/app.py
@@ -58,4 +58,4 @@ def download_file(filename):
     return send_from_directory('output', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
